a90df4ad86b8a45b47330ade88d54a68b96fc781
Evan <Evan@DESKTOP-CV292H4.gateway.pace.com>
2018-05-07 21:12:34 -0700

    no

--- APCS Final/src/DrawingSurface.java
+++ APCS Final/src/DrawingSurface.java
@@ -21,7 +21,8 @@ public class DrawingSurface extends PApplet {
 		board = new Grid();
 
 		
-		music = new AudioClip("music" + System.getProperty("file.separator") + "tibsun");
+		music = new AudioClip("music" + System.getProperty("file.separator") + "menu");
+		music.setCycleCount(AudioClip.INDEFINITE); //menu music will infinite loop
 	}
 	
 	// The statements in the setup() function 
@@ -41,51 +42,8 @@ public class DrawingSurface extends PApplet {
 		textAlign(LEFT);
 		textSize(12);
 		
-		String message = "";
-		
-		int turnMod = board.getTurn() % 8;
-		
-		if (turnMod == 1) {//P1 viewing their own player grid
-			message = "Here is the current state of your fleet, Player 1.\nCyan squares are living ships, red squares are destroyed ships."
-					+ "\n\nClick on the grid to continue";
-		}
-		else if (turnMod == 2) {//P1 viewing their target grid.
-			message = "Here is your targeting board, Player 1.\nWhite squares are uncovered, red squares are hits, and yellow squares are misses."
-					+ "\n\nClick a square to fire on it.";
-			
-		}
-		
-		else if (turnMod == 3) {//P1 viewing their target grid.
-			message = "Boom! Here is the result of your attack, Player 1.\nWhite squares are uncovered, red squares are hits, and yellow squares are misses."
-					+ "\n\nClick on the grid to continue";
-			
-		}
-		
-		else if (turnMod == 4) {//Transition
-			message = "GIVE CONTROL TO PLAYER 2. Unless you're playing against yourself, I don't judge. \n\nClick on the grid area to continue.";
-		}
-		
-		else if (turnMod == 5) {//P2 viewing their own player grid
-			message = "Here is the current state of your fleet, Player 2.\nCyan squares are living ships, red squares are destroyed ships."
-					+ "\n\nClick on the grid to continue";
-		}
-		
-		else if (turnMod == 6) {//P2 viewing their target grid.
-			message = "Here is your targeting board, Player 2.\nWhite squares are uncovered, red squares are hits, and yellow squares are misses."
-					+ "\n\nClick a square to fire on it.";
-			
-		}
-		
-		else if (turnMod == 7) {//P2 viewing their target grid.
-			message = "Boom! Here is the result of your attack, Player 2.\nWhite squares are uncovered, red squares are hits, and yellow squares are misses."
-					+ "\n\nClick on the grid to continue";
-			
-		}
-		
-		else if (turnMod == 0) {//Transition.
-			message = "GIVE CONTROL TO PLAYER 1. Unless you're playing against yourself, I don't judge. \n\nClick on the grid area to continue.";
-		}
-		
+		String message = "";	
+
 		text(message, height+20, 30);
 		
 		
