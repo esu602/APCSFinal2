cf2dc6fbffb12c957dc546943540f8b63bfd8a1e
Ryan Yoshida <33506695+ryoshida28@users.noreply.github.com>
2018-05-15 08:39:47 -0700

    Add files via upload
    Big Changes
    
    Changes:
    1) Player class has been reorganized
    2) DrawingSurface has been made compatible with Player class
    3) Screen switching capabilities applied to DrawingSurface
    4) Redesigned file system to create easier access to images
    
    TODO:
    1) Fix Enemy class: I suggest a super class called "Actor" which Player and Enemy both extend. This will allow for less duplicated code. I do not see an necessity for an interface at this point.
    2) Restructure Menu, use some form of layout.
    3) Continue work on health status boxes on right.
    4) Fix Combat class and combat variable in Drawing Surface.
    
    Other Notes:
    1) Hit Ctrl + Shift + F every now and then
    2) Avoid usage of fileio in a repeated method such as draw(); eg. loadImage()
    3) Avoid putting too much code in DrawingSurface. Try to distribute it throughout other classes in a manner that makes sense.
    4) Make comments on unexplained variables, methods, etc. so that others know what their purpose is. For example, the combat variable is DrawingSurface is completely unexplained.
    5) Highly suggest using packages

--- APCS Final2/src/DrawingSurface.java
+++ APCS Final2/src/DrawingSurface.java
@@ -1,9 +1,7 @@
 
 import javafx.scene.media.*;
 
-import java.awt.Point;
 import java.awt.event.KeyEvent;
-import java.awt.geom.Point2D;
 import java.nio.file.Paths;
 
 import processing.core.PApplet;
@@ -11,230 +9,202 @@ import processing.core.PImage;
 
 /**
  * 
- * This class draws the player, grid and other npcs onto a board, also handles the GUI, music and visual representation of the game
+ * This class draws the player, grid and other npcs onto a board, also handles
+ * the GUI, music and visual representation of the game
  * 
  * 
  * @author David Dominique ,Evan Su
- *@version 5/9/18
+ * @version 5/9/18
  */
 
 public class DrawingSurface extends PApplet {
 
+	public static final int STAGE_MENU 		= 0;
+	public static final int STAGE_BATTLE 	= 1;
+	public static final int STAGE_END		= 2;
+	
+	private PImage menuScreen, battleScreen, endScreen;
+	private int stage;
+	
 	private Grid board;
-	
+
 	private Player playerTest;
-	
-	private PImage playerImg;
-	
-	private int X,Y;
-	
+	private Enemy enemy;
+
+
+	// This should be kept in Player
+	// private int X,Y;
+
 	private int healthX, healthY;
 	private int decreaseHealthX, decreaseHealthY;
-	
+
 	private AudioClip music;
 
-	
-	
-	
-	
 	public DrawingSurface() {
 		board = new Grid();
-		X=5;
-		Y=5;
-		
-		healthX= width+1125;
-		healthY=height-10;
-		
-		decreaseHealthX=width+1125;
-		decreaseHealthY=height+200;
-		
-		
-		playerTest = new Player(board);
 
-		
+		healthX = width + 1125;
+		healthY = height - 10;
+
+		decreaseHealthX = width + 1125;
+		decreaseHealthY = height - 10;
+
+		playerTest = new Player(board, 5, 5);
+		//enemy = new Enemy(board);
+
 		music = new AudioClip(Paths.get("music/menu.mp3").toUri().toString());
 		music.play();
+		
+		
+		stage = STAGE_MENU;
 	}
-	
-	// The statements in the setup() function 
+
+	// The statements in the setup() function
 	// execute once when the program begins
 	public void setup() {
+		String backgroundPath = "images" + Main.FILE_SEPARATOR + "background" + Main.FILE_SEPARATOR;
+		menuScreen 		= loadImage(backgroundPath + "scene_menu.png");
+		endScreen 		= loadImage(backgroundPath + "scene_end.jpg");
+		battleScreen 	= loadImage(backgroundPath + "scene_battle.png");
 		
-		System.out.println("this is the setup method");
-
-		playerImg = loadImage("sprites/bug.gif");
-		playerImg.resize(height/board.getDim(), height/board.getDim());
-		//size(0,0,PApplet.P3D);
+		String spritePath = "images" + Main.FILE_SEPARATOR + "sprites" + Main.FILE_SEPARATOR;
+		playerTest.setImage(loadImage(spritePath + "sprite_player.png"));
 	}
-	
-	// The statements in draw() are executed until the 
-	// program is stopped. Each statement is executed in 
-	// sequence and after the last line is read, the first 
+
+	// The statements in draw() are executed until the
+	// program is stopped. Each statement is executed in
+	// sequence and after the last line is read, the first
 	// line is executed again.
-	public void draw() { 
-			
+	public void draw() {
+
+		// SET MUSIC
 		if (!music.isPlaying()) {
-			
 			if (board != null) {
 				playMusic("random");
 			}
-			
-		}
-
-		
-		background(255);   // Clear the screen with a white background
-		//fill(0);
-		textAlign(LEFT);
-		textSize(12);
-		
-		String message = "";	
-
-		text(message, height+20, 30);
-		
-		String message2 = playerTest.healthToString();
-		
-		
-		if (playerTest.getHealth()==100)
-		{
-			//Max Health warning
-			text (playerTest.maxHealth(),height+40,50);
-			//Health Indicator
-			text(message2, height+40,10);
-
-			
-		}
-		
-		else
-		{
-			text(message2, height+40,50);
-
 		}
 		
 		
-		
-		
+		// DETERMINE STAGE
+		if (stage == STAGE_MENU) {
+			image(menuScreen, 0, 0, width, height);
+			
+			// SETUP MENU CONTENTS
+			textSize(56);
+			textAlign(CENTER);
+			text("Press Enter To Start", width / 2 - 25, height / 3 - 20);
 
-		
-		//where the board is drawn
-		if (board != null) {
-			board.draw(this, 0, 0, height, height);
-			//Health Up Box
-			rect (healthX,healthY,200,200,5);
-			//Health Down Box
+			textAlign(CENTER);
+			text("Instructions" + "\n" + "W-Move Player Up" + "\n" + "A-Move Player Left" + "\n" + "S-Move Player Down"
+					+ "\n" + "D-Move Player Right", width / 2, height / 2);
+		} else if (stage == STAGE_BATTLE) {
+			//SETUP BATTLE SCREEN
+			background(255); // Clear the screen with a white background
+			fill(0);
+			textAlign(LEFT);
+			textSize(12);
 			
-			rect (decreaseHealthX,decreaseHealthY,200,200,5);
-
+			// MAX HEALTH WARNING
+			if (playerTest.getHealth() == 100) {
+				text("MAX HEALTH", 1125, 175);
+			} else if (!playerTest.isAlive()) {		// Player is dead
+				stage = STAGE_END;
+			}
 			
+			// Draw Board
+			if (board != null) {
+				board.draw(this, 0, 0, height, height);
+			}
 			
+			// Health Square
+			noFill();
+			rect(healthX, healthY, 200, 100, 5);
+			String message2 = "Player Health: " + playerTest.getHealth();
+			text(message2, 1110, 150);
+			text("Press button for health", 1255, 145);
+			// Attack Button
+			noFill();
+			rect(healthX, healthY + 150, 200, 100, 5);
+			text("Attack With Selected Weapon", 1240, 290);
+			// Cycle Weapon Buttons Left & Right
+			noFill();
+			text("Cycle Weapon", 1285, 390);
+			rect(healthX, healthY + 300, 50, 50, 5);
+			noFill();
+			rect(healthX + 150, healthY + 300, 50, 50);
+			// Defend Button
+			noFill();
+			text("Defend With Selected Item", 1250, 535);
+			rect(healthX, healthY + 390, 200, 100);
+			// Cycle Armor Buttons
+			noFill();
+			text("Cycle Armor", 1290, 625);
+			rect(healthX, healthY + 525, 50, 50, 5);
+			noFill();
+			rect(healthX + 150, healthY + 525, 50, 50);
+		} else if (stage == STAGE_END) {
+			image(endScreen, 0, 0, width, height);
 		}
-		
-		
-		
-		//player start posistion
-		
-		board.setState(X, Y, 3);
-		
-		
+
 	}
-	
+
 	public void keyPressed() {
 
-		if (keyCode == KeyEvent.VK_W) 
-		{
-			
-			if (playerTest.canMoveUp(X, Y)==true)
-			{
-				playerTest.moveUp(X, Y);
-				
-			 Y--;
-
-			}
-			
-			
-			
-			
+		// MENU CONTROL
+		if(stage == STAGE_MENU && keyCode == KeyEvent.VK_ENTER) {
+			stage = STAGE_BATTLE;
 		}
 		
-		else if (keyCode==KeyEvent.VK_A)
-		{
-			if (playerTest.canMoveLeft(X, Y)==true)
-			{
-				playerTest.moveLeft(X, Y);
-				
-				X--;
-
-			}
+		// HANDLE PLAYER MOVEMENT
+		int toX = playerTest.getX();
+		int toY = playerTest.getY();
+		if (keyCode == KeyEvent.VK_W) {
+			toY--;
+		} else if (keyCode == KeyEvent.VK_A) {
+			toX--;
+		} else if (keyCode == KeyEvent.VK_S) {
+			toY++;
+		} else if (keyCode == KeyEvent.VK_D) {
+			toX++;
 		}
 		
-		else if (keyCode==KeyEvent.VK_S)
-		{
-			if (playerTest.canMoveDown(X, Y)==true)
-			{
-				playerTest.moveDown(X, Y);
-				Y++;
-
-			}
-		}
-		
-		else if (keyCode==KeyEvent.VK_D)
-		{
-			if (playerTest.canMoveRight(X, Y)==true)
-			{
-				playerTest.moveRight(X, Y);
-				X++;
-
-			}
+		if(playerTest.canMoveTo(toX, toY)) {
+			playerTest.moveTo(toX, toY);
 		}
 
-		}
-
-	//cap health to 100, cant go above it
-	public void mousePressed() {
-		  if(mouseX>healthX && mouseX <healthX+200 && mouseY>healthY && mouseY <healthY+200){
-			  
-			  if (playerTest.getHealth()<100)
-			  playerTest.setHealth(playerTest.getHealth()+1);
-			  
-		  }
-		  
-		  if(mouseX>decreaseHealthX && mouseX <decreaseHealthX+200 && mouseY>decreaseHealthY && mouseY <decreaseHealthY+200){
-			  
-			  playerTest.setHealth(playerTest.getHealth()-1);
-			  
-		  }
-		  
-		  
 	}
-	
+
+	// cap health to 100, cant go above it
+	public void mousePressed() {
+		if (mouseX > healthX && mouseX < healthX + 200 && mouseY > healthY && mouseY < healthY + 200) {
+
+			if (playerTest.getHealth() < 100)
+				playerTest.setHealth(playerTest.getHealth() + 1);
+
+		}
+
+		if (mouseX > decreaseHealthX && mouseX < decreaseHealthX + 200 && mouseY > decreaseHealthY
+				&& mouseY < decreaseHealthY + 200) {
+
+			playerTest.setHealth(playerTest.getHealth() - 1);
+
+		}
+
+	}
+
 	public void playMusic(String filename) {
 		if (filename.equals("random")) {
 			double d = Math.random();
 			System.out.println(d);
 			if (d < 0.3) {
 				filename = "halo3";
-			}
-			else if (d < 0.6) {
+			} else if (d < 0.6) {
 				filename = "tibdawn";
-			}
-			else {
+			} else {
 				filename = "menu";
 			}
 		}
 		music = new AudioClip(Paths.get("music/" + filename + ".mp3").toUri().toString());
 		music.play();
 	}
-
-	public PImage getPlayerImage() {
-		return playerImg;
-	}
 }
-
-
-
-
-
-
-
-
-
-
--- APCS Final2/src/Enemy.java
+++ APCS Final2/src/Enemy.java
@@ -1,4 +1,9 @@
 //represents an enemy
 public class Enemy {
 
+	private Grid enemyGrid;
+	
+	public Enemy(Grid grid) {
+		enemyGrid = grid;
+	}
 }
