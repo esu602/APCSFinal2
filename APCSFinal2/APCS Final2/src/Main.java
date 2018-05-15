6d830f7b2791883285b4e5214a1be776164fa0e8
Ryan Yoshida <33506695+ryoshida28@users.noreply.github.com>
2018-05-15 00:19:29 -0700

    Add files via upload
    Big Changes
    
    Changes:
    1) Player class has been reorganized
    2) DrawingSurface has been made compatible with Player class
    3) Screen switching capabilities applied to DrawingSurface
    4) Redesigned file system to create easier access to images
    
    TODO:
    1) Fix Enemy class: I suggest a super class called "Actor" which Player and Enemy both extend. This will allow for less duplicated code. I do not see an necessity for an interface at this point.
    2) Restructure Menu, use some form of layout.
    3) Continue work on health status boxes on right.
    4) Fix Combat class and combat variable in Drawing Surface.
    
    Other Notes:
    1) Hit Ctrl + Shift + F every now and then
    2) Avoid usage of fileio in a repeated method such as draw(); eg. loadImage()
    3) Avoid putting too much code in DrawingSurface. Try to distribute it throughout other classes in a manner that makes sense.
    4) Make comments on unexplained variables, methods, etc. so that others know what their purpose is. For example, the combat variable is DrawingSurface is completely unexplained.
    5) Highly suggest using packages

---
+++ APCSFinal2/APCS Final2/src/Main.java
@@ -0,0 +1,46 @@
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
+/**
+ * 
+ * This class is the Main class from which the program is launched
+ * 
+ * @author David Dominique,Evan Su
+ * @version 5/9/18
+ */
+public class Main extends JFrame {
+	
+	public static final Dimension DEFAULT_DIMENSIONS = Toolkit.getDefaultToolkit().getScreenSize();
+
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final String FILE_SEPARATOR = System.getProperty("file.separator");
+
+	public static void main(String args[]) {
+
+		DrawingSurface drawing = new DrawingSurface();
+		PApplet.runSketch(new String[]{""}, drawing);
+		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		JFrame window = (JFrame)canvas.getFrame();
+
+		// Centers window on screen.
+		window.setBounds((int) (Main.DEFAULT_DIMENSIONS.getWidth()/4),
+				(int) (Main.DEFAULT_DIMENSIONS.getHeight()/4),
+				(int) (Main.DEFAULT_DIMENSIONS.getWidth()/2),
+				(int) (Main.DEFAULT_DIMENSIONS.getHeight()/2));
+		window.setMinimumSize(new Dimension(100,100));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+
+		window.setVisible(true);
+		canvas.requestFocus();
+		
+	}
+}
